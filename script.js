f8522f5dec8ed2eb12297d5a232770e4d79bb52f
Carly Peyton <carlypeyton@gmail.com>
2021-01-13 14:33:07 -0500

    Create function for search button. Displays current weather. Need to add UV index and forecast weather AJAX requests.

--- script.js
+++ script.js
@@ -14,9 +14,62 @@ $("#current-weather").hide();
 $(".list-group-item").hide();
 
 //Enter key for searching as well as search button
-$("#city-search").keypress(function(event) { 
-    if (event.keyCode === 13) { 
+$("#city-search").keypress(function (event) {
+    if (event.keyCode === 13) {
         // event.preventDefault();
-        $("#search-button").click(); 
-    } 
+        $("#search-button").click();
+    }
+});
+
+//Function for search button click event
+searchButton.click(function () {
+    //Show hidden containers
+    $(".history-container").show();
+    $("#forecast").show();
+    $("#current-weather").show();
+    //Variable for city user is searching
+    let citySearch = $("#city-search").val();
+    // Variable for current weather  
+    let currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&Appid=" + APIKey + "&units=imperial";
+    //As long as the city search is not an empty string
+    if (citySearch == "") {
+        return citySearch;
+    } else {
+        //log the searched city to console
+        console.log(citySearch);
+        //AJAX get request for current weather 
+        $.ajax({
+            url: currentWeatherAPI,
+            method: "GET"
+        }).then(function (response) {
+            //Get rid of border and white background around list group
+            $(".list-group").attr('style', 'border: none');
+            $(".list-group").addClass('bg-light');
+            //Add class to list group and prepend list item with list-group-items for each city
+            let searchHistory = $(".list-group").addClass("list-group-item");
+            searchHistory.prepend("<button type='button' class='list-group-item list-group-item-action city-button'>" + response.name + "</button>");
+            localStorage.setItem(searchNumber, response.name);
+            //Clear searched term from input field
+            $("#city-search").val('');
+            searchNumber = searchNumber + 1;
+            //Current Weather
+            //Convert temperature to fahrenheit, fix to 1 decimal place
+            let tempF = (response.main.temp).toFixed(1);
+            //transfer content to HTML
+            $("#city-name").html(response.name);
+            $("#temperature").text(tempF + " °F");
+            $("#humidity").text(response.main.humidity + "%");
+            $("#wind-speed").text(response.wind.speed + " MPH");
+            //Set current date*******************
+            let currentDay = moment().format(" (M/D/YYYY)");
+            $(".current-date").append(currentDay);
+            $(".current-date").append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" id="current-weather-icon">`);
+            //Log to console to check functionality
+            console.log(response);
+            console.log(response.name);
+            console.log(response.main.temp);
+            console.log(response.main.humidity);
+            console.log(response.wind.speed);
+        });
+    }
 });
