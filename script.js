0dfb1a0eebb8f58d160c7c8849e36fd558ebb72c
Carly Peyton <carlypeyton@gmail.com>
2021-01-14 10:06:52 -0500

    Created click even function for city buttons in search history. Buttons now functional. User can click previously searched for city, and current weather/forecast will update to that city.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/////TO-DO
+//function to display last searched for city on page open
+//validation
+
+
 //Define variables, set Jquery for search button, and API key for Open Weather API
 var searchButton = $("#search-button");
 var clearButton = $("#clear-button");
@@ -31,7 +36,7 @@ searchButton.click(function () {
     // Variable for current weather  
     let currentWeatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&Appid=" + APIKey + "&units=imperial";
     //As long as the city search is not an empty string
-    if (citySearch == "") {
+    if (citySearch === "") {
         return citySearch;
     } else {
         //log the searched city to console
@@ -41,16 +46,49 @@ searchButton.click(function () {
             url: currentWeatherAPI,
             method: "GET"
         }).then(function (response) {
+            //Log to console to check functionality
+            console.log(response);
+            console.log(response.name);
+            console.log(response.main.temp);
+            console.log(response.main.humidity);
+            console.log(response.wind.speed);
             //Get rid of border and white background around list group
             $(".list-group").attr('style', 'border: none');
             $(".list-group").addClass('bg-light');
             //Add class to list group and prepend list item with list-group-items for each city
             let searchHistory = $(".list-group").addClass("list-group-item");
             searchHistory.prepend("<button type='button' class='list-group-item list-group-item-action city-button'>" + response.name + "</button>");
+            //Clicking on city button in search history gets weather/forecast for that city
+            $(".city-button").on("click", function () {
+                let city = $(this).text();
+                console.log($(this).text());
+                let cityButtonAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&Appid=" + APIKey + "&units=imperial";
+                //Log value to console to ensure functionality
+                $.ajax({
+                    url: cityButtonAPI,
+                    method: "GET"
+                }).then(function (response) {
+                    //Log to console to check functionality
+                    console.log(response);
+                    console.log(response.name);
+                    console.log(response.main.temp);
+                    console.log(response.main.humidity);
+                    console.log(response.wind.speed); $(this).text('');
+                    //transfer content to HTML
+                    $("#city-name").html(response.name);
+                    $("#temperature").text(tempF + " °F");
+                    $("#humidity").text(response.main.humidity + "%");
+                    $("#wind-speed").text(response.wind.speed + " MPH");
+                    //Set current date*******************
+                    let currentDay = moment().format(" (M/D/YYYY)");
+                    $(".current-date").append(currentDay);
+                    $(".current-date").append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" id="current-weather-icon">`);
+                });
+            });
+            //Save search to local storage
             localStorage.setItem('user-search', response.name);
             //Clear searched term from input field
             $("#city-search").val('');
-            //Current Weather
             //Convert temperature to fahrenheit, fix to 1 decimal place
             let tempF = (response.main.temp).toFixed(1);
             //transfer content to HTML
@@ -62,13 +100,7 @@ searchButton.click(function () {
             let currentDay = moment().format(" (M/D/YYYY)");
             $(".current-date").append(currentDay);
             $(".current-date").append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" id="current-weather-icon">`);
-            //Log to console to check functionality
-            console.log(response);
-            console.log(response.name);
-            console.log(response.main.temp);
-            console.log(response.main.humidity);
-            console.log(response.wind.speed);
-
+            
             // Variable for current weather UV index
             let currentWeatherUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
             //AJAX get request for current weather UV index
@@ -139,10 +171,6 @@ clearButton.click(function () {
 });
 
 
-
-
-
-
 ////////////////////////FROM HERE BELOW TO GO////////////////////////////
 
 //Function to load last city searched for
@@ -150,15 +178,9 @@ function loadLastCity() {
 }
 
 
-
-//Function for user to search weather for item in search history
-// cityButton.click(function() {
-// });
-
-
 /////////////////////////RUN TIME/////////////////////////////////////////
 
 //When window loads show last searched city
-// $(window).on("load", loadLastCity) 
+$(window).on("load", loadLastCity)
 
 
