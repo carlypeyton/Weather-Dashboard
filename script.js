7c76d074e09428fdc816d8e179c04759d57536af
Carly Peyton <carlypeyton@gmail.com>
2021-01-16 09:55:39 -0500

    Updated script so that containers are hidden when user first load page if there is nothing saved in local storage.

--- script.js
+++ script.js
@@ -225,85 +225,93 @@ clearButton.click(function () {
 ////////////////////////FROM HERE BELOW TO GO////////////////////////////
 
 //Function to load last city searched for weather and forecast
-//Need to hide containers unless local storage !== NULL
 window.onload = function loadLastCity() {
     let lastCity = localStorage.getItem('user-search');
-    //Show hidden containers
-    $(".history-container").show();
-    $("#forecast").show();
-    $("#current-weather").show();
     console.log(lastCity);
-    let lastCityAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + lastCity + "&Appid=" + APIKey + "&units=imperial";
-    $.ajax({
-        url: lastCityAPI,
-        method: "GET"
-    }).then(function (response) {
-        //transfer content to HTML
-        $("#city-name").html(response.name);
-        let tempF = (response.main.temp).toFixed(1);
-        $("#temperature").text(tempF + " °F");
-        $("#humidity").text(response.main.humidity + "%");
-        $("#wind-speed").text(response.wind.speed + " MPH");
-        //Set current date
-        let currentDay = moment().format(" (M/D/YYYY)");
-        $(".current-date").append(currentDay);
-        $(".current-date").append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" id="current-weather-icon">`);
-        // Variable for current weather UV index
-        let lastCityUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
-        //AJAX get request for city button UV index
+   //ON PAGE LOAD, IF THE VALUE OF LAST CITY IS NOT NULL, THEN DO _____ THIS. IF NULL, THEN HIDE CONTAINERS
+    if (localStorage.getItem("user-search") === null) {
+        $(".history-container").hide();
+        $("#forecast").hide();
+        $("#current-weather").hide();
+        $(".list-group-item").hide();
+        return;
+    } else {
+        let lastCityAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + lastCity + "&Appid=" + APIKey + "&units=imperial";
         $.ajax({
-            url: lastCityUV,
+            url: lastCityAPI,
             method: "GET"
         }).then(function (response) {
-            //Set value of UV index
-            $("#uv-index").empty();
-            $("#uv-index").text(response.value);
-            let uvindex = response.value;
-            //Change color of background on UV index based on scale
-            if (uvindex <= 3) {
-                $("#uv-index").addClass("low");
-                $("#uv-index").removeClass("medium");
-                $("#uv-index").removeClass("high");
-                $("#uv-index").removeClass("very-high");
-            } else if (uvindex >= 3 && uvindex <= 6) {
-                $("#uv-index").addClass("medium");
-                $("#uv-index").removeClass("low");
-                $("#uv-index").removeClass("high");
-                $("#uv-index").removeClass("very-high");
-            } else if (uvindex >= 6 && uvindex <= 8) {
-                $("#uv-index").addClass("high");
-                $("#uv-index").removeClass("medium");
-                $("#uv-index").removeClass("low");
-                $("#uv-index").removeClass("very-high");
-            } else {
-                $("#uv-index").addClass("very-high");
-                $("#uv-index").removeClass("medium");
-                $("#uv-index").removeClass("high");
-                $("#uv-index").removeClass("low");
-            }
+            //Show hidden containers
+            $(".history-container").show();
+            $("#forecast").show();
+            $("#current-weather").show();
+            //transfer content to HTML
+            $("#city-name").html(response.name);
+            let tempF = (response.main.temp).toFixed(1);
+            $("#temperature").text(tempF + " °F");
+            $("#humidity").text(response.main.humidity + "%");
+            $("#wind-speed").text(response.wind.speed + " MPH");
+            //Set current date
+            let currentDay = moment().format(" (M/D/YYYY)");
+            $(".current-date").append(currentDay);
+            $(".current-date").append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" id="current-weather-icon">`);
+            // Variable for current weather UV index
+            let lastCityUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+            //AJAX get request for city button UV index
+            $.ajax({
+                url: lastCityUV,
+                method: "GET"
+            }).then(function (response) {
+                //Set value of UV index
+                $("#uv-index").empty();
+                $("#uv-index").text(response.value);
+                let uvindex = response.value;
+                //Change color of background on UV index based on scale
+                if (uvindex <= 3) {
+                    $("#uv-index").addClass("low");
+                    $("#uv-index").removeClass("medium");
+                    $("#uv-index").removeClass("high");
+                    $("#uv-index").removeClass("very-high");
+                } else if (uvindex >= 3 && uvindex <= 6) {
+                    $("#uv-index").addClass("medium");
+                    $("#uv-index").removeClass("low");
+                    $("#uv-index").removeClass("high");
+                    $("#uv-index").removeClass("very-high");
+                } else if (uvindex >= 6 && uvindex <= 8) {
+                    $("#uv-index").addClass("high");
+                    $("#uv-index").removeClass("medium");
+                    $("#uv-index").removeClass("low");
+                    $("#uv-index").removeClass("very-high");
+                } else {
+                    $("#uv-index").addClass("very-high");
+                    $("#uv-index").removeClass("medium");
+                    $("#uv-index").removeClass("high");
+                    $("#uv-index").removeClass("low");
+                }
+            });
+            // Variable for 5 day forecast 
+            let lastCityForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + lastCity + "&Appid=" + APIKey + "&units=imperial";
+            //AJAX get request for forecast weather 
+            $.ajax({
+                url: lastCityForecast,
+                method: "GET"
+            }).then(function (response) {
+                //For loop for 5 days
+                var i;
+                for (i = 0; i < 5; i++) {
+                    //Define variables for responses
+                    let date = new Date((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
+                    let iconcode = response.list[((i + 1) * 8) - 1].weather[0].icon;
+                    let iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+                    let tempF = (response.list[((i + 1) * 8) - 1].main.temp).toFixed(1);
+                    let humidity = response.list[((i + 1) * 8) - 1].main.humidity;
+                    //Update HTML with date, forecast image, and weather
+                    $("#forecast-date" + i).html(date);
+                    $("#forecast-image" + i).html("<img src=" + iconurl + ">");
+                    $("#forecast-temperature" + i).html(tempF + " °F");
+                    $("#forecast-humidity" + i).html(humidity + "%");
+                }
+            });
         });
-        // Variable for 5 day forecast 
-        let lastCityForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + lastCity + "&Appid=" + APIKey + "&units=imperial";
-        //AJAX get request for forecast weather 
-        $.ajax({
-            url: lastCityForecast,
-            method: "GET"
-        }).then(function (response) {
-            //For loop for 5 days
-            var i;
-            for (i = 0; i < 5; i++) {
-                //Define variables for responses
-                let date = new Date((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
-                let iconcode = response.list[((i + 1) * 8) - 1].weather[0].icon;
-                let iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
-                let tempF = (response.list[((i + 1) * 8) - 1].main.temp).toFixed(1);
-                let humidity = response.list[((i + 1) * 8) - 1].main.humidity;
-                //Update HTML with date, forecast image, and weather
-                $("#forecast-date" + i).html(date);
-                $("#forecast-image" + i).html("<img src=" + iconurl + ">");
-                $("#forecast-temperature" + i).html(tempF + " °F");
-                $("#forecast-humidity" + i).html(humidity + "%");
-            }
-        });
-    });
-} 
+    }
+}
